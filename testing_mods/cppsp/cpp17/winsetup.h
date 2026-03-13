90631327f53d3f24870dad640bed42b3bd451aee
user19870 <87924678+user19870@users.noreply.github.com>
2026-03-13 12:54:57 +0800

    Add files via upload

---
+++ testing_mods/cppsp/cpp17/winsetup.h
@@ -0,0 +1,12 @@
+#if defined(_WIN32) || defined(_WIN64)
+
+typedef unsigned int UINT;
+typedef int BOOL;
+
+extern "C" BOOL __stdcall SetConsoleOutputCP(UINT wCodePageID);
+extern "C" BOOL __stdcall SetConsoleCP(UINT wCodePageID);
+
+ #else
+//have no requirement for other platforms theorically
+
+#endif
